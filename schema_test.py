09176410053b688e1e2c345fff4f491ce38a38da
Carl Wilkerson <112122578+CarlCrossbones@users.noreply.github.com>
2025-02-10 15:06:10 -0600

    Create JSON Schema (#29)
    * finished task
    
    * addressing feedback

---
+++ schema_test.py
@@ -0,0 +1,236 @@
+import json
+import jsonschema
+from jsonschema import validate
+
+with open('schema.json', 'r') as file:
+    schema = json.load(file)
+
+## Correct Input (Output: Valid!)
+
+json_test = [{
+    "Financial Aid Offered Amount": 34000.0,
+    "incoming_text_count": 12,
+    "outgoing_text_count": 12,
+    "phone_successful_count": 12,
+    "phone_unsuccessful_count": 12,
+    "phone_voicemail_count": 12,
+    "Admitted Students Day": 0,
+    "Bison Day": 0,
+    "Bison Day @ The Weekend": 0,
+    "Campus Visit": 0,
+    "Dallas Bison Exclusive": 0,
+    "Football Visit": 0,
+    "Golf Visit": 0,
+    "Oklahoma City Bison Exclusive": 0,
+    "Scholars Bison Day": 0,
+    "Scholars Mixer and Banquet": 0,
+    "Scholarship Interview": 0,
+    "Scholarship Interview Registration": 0,
+    "Softball Visit": 0,
+    "Track Visit": 0,
+    "Tulsa Bison Exclusive": 0,
+    "Volleyball Visit": 0,
+    "Events Attended Count": 0,
+    "Country": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "State": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Gender": [0, 1],
+    "Ethnicity": [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Origin Source": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Student Type": [0, 0, 0, 0, 1],
+    "Major": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],   
+    "Athlete": 0,
+    "Sport": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Raley College Tag Exists": 0,
+    "Recruiting Territory": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Counselor": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+}]
+
+try:
+    validate(instance=json_test, schema=schema)
+    print("Valid!")
+except jsonschema.exceptions.ValidationError as err:
+    print("Invalid!")
+
+## Missing value (Output: Invalid!)
+
+json_test = [{
+    "Financial Aid Offered Amount": 34000.0,
+    "incoming_text_count": 12,
+    "outgoing_text_count": 12,
+    "phone_successful_count": 12,
+    "phone_unsuccessful_count": 12,
+    "phone_voicemail_count": 12,
+    "Admitted Students Day": 0,
+    "Bison Day": 0,
+    "Bison Day @ The Weekend": 0,
+    "Campus Visit": 0,
+    "Dallas Bison Exclusive": 0,
+    "Golf Visit": 0,
+    "Oklahoma City Bison Exclusive": 0,
+    "Scholars Bison Day": 0,
+    "Scholars Mixer and Banquet": 0,
+    "Scholarship Interview": 0,
+    "Scholarship Interview Registration": 0,
+    "Softball Visit": 0,
+    "Track Visit": 0,
+    "Tulsa Bison Exclusive": 0,
+    "Volleyball Visit": 0,
+    "Events Attended Count": 0,
+    "Country": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "State": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Gender": [0, 1],
+    "Ethnicity": [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Origin Source": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Student Type": [0, 0, 0, 0, 1],
+    "Major": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],   
+    "Athlete": 0,
+    "Sport": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Raley College Tag Exists": 0,
+    "Recruiting Territory": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Counselor": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+}]
+
+try:
+    validate(instance=json_test, schema=schema)
+    print("Valid!")
+except jsonschema.exceptions.ValidationError as err:
+    print("Invalid!")
+
+## Type Error (Output: Invalid!)
+
+json_test = [{
+    "Financial Aid Offered Amount": 34000.0,
+    "incoming_text_count": 12,
+    "outgoing_text_count": 12,
+    "phone_successful_count": 12,
+    "phone_unsuccessful_count": 12,
+    "phone_voicemail_count": 12,
+    "Admitted Students Day": 0,
+    "Bison Day": 0,
+    "Bison Day @ The Weekend": 0,
+    "Campus Visit": 0,
+    "Dallas Bison Exclusive": 0,
+    "Football Visit": 0,
+    "Golf Visit": 0,
+    "Oklahoma City Bison Exclusive": 0,
+    "Scholars Bison Day": 0,
+    "Scholars Mixer and Banquet": 0,
+    "Scholarship Interview": 0,
+    "Scholarship Interview Registration": 0,
+    "Softball Visit": 0,
+    "Track Visit": 0,
+    "Tulsa Bison Exclusive": 0,
+    "Volleyball Visit": 0,
+    "Events Attended Count": 0,
+    "Country": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "State": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Gender": "[0, 1]",
+    "Ethnicity": [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Origin Source": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Student Type": [0, 0, 0, 0, 1],
+    "Major": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],   
+    "Athlete": 0,
+    "Sport": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Raley College Tag Exists": 0,
+    "Recruiting Territory": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Counselor": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+}]
+
+try:
+    validate(instance=json_test, schema=schema)
+    print("Valid!")
+except jsonschema.exceptions.ValidationError as err:
+    print("Invalid!")
+
+## Correct Input: Multiple entries (Output: Valid!)
+
+json_test = [{
+    "Financial Aid Offered Amount": 34000.0,
+    "incoming_text_count": 12,
+    "outgoing_text_count": 12,
+    "phone_successful_count": 12,
+    "phone_unsuccessful_count": 12,
+    "phone_voicemail_count": 12,
+    "Admitted Students Day": 0,
+    "Bison Day": 0,
+    "Bison Day @ The Weekend": 0,
+    "Campus Visit": 0,
+    "Dallas Bison Exclusive": 0,
+    "Football Visit": 0,
+    "Golf Visit": 0,
+    "Oklahoma City Bison Exclusive": 0,
+    "Scholars Bison Day": 0,
+    "Scholars Mixer and Banquet": 0,
+    "Scholarship Interview": 0,
+    "Scholarship Interview Registration": 0,
+    "Softball Visit": 0,
+    "Track Visit": 0,
+    "Tulsa Bison Exclusive": 0,
+    "Volleyball Visit": 0,
+    "Events Attended Count": 0,
+    "Country": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "State": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Gender": [0, 1],
+    "Ethnicity": [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Origin Source": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Student Type": [0, 0, 0, 0, 1],
+    "Major": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],   
+    "Athlete": 0,
+    "Sport": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Raley College Tag Exists": 0,
+    "Recruiting Territory": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Counselor": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+},
+{
+    "Financial Aid Offered Amount": 34000.0,
+    "incoming_text_count": 12,
+    "outgoing_text_count": 12,
+    "phone_successful_count": 13,
+    "phone_unsuccessful_count": 12,
+    "phone_voicemail_count": 12,
+    "Admitted Students Day": 0,
+    "Bison Day": 0,
+    "Bison Day @ The Weekend": 0,
+    "Campus Visit": 0,
+    "Dallas Bison Exclusive": 0,
+    "Football Visit": 0,
+    "Golf Visit": 0,
+    "Oklahoma City Bison Exclusive": 0,
+    "Scholars Bison Day": 0,
+    "Scholars Mixer and Banquet": 0,
+    "Scholarship Interview": 0,
+    "Scholarship Interview Registration": 0,
+    "Softball Visit": 0,
+    "Track Visit": 0,
+    "Tulsa Bison Exclusive": 0,
+    "Volleyball Visit": 0,
+    "Events Attended Count": 0,
+    "Country": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "State": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Gender": [0, 1],
+    "Ethnicity": [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Origin Source": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Student Type": [0, 0, 0, 0, 1],
+    "Major": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],   
+    "Athlete": 0,
+    "Sport": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Raley College Tag Exists": 0,
+    "Recruiting Territory": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "Counselor": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+}]
+
+try:
+    validate(instance=json_test, schema=schema)
+    print("Valid!")
+except jsonschema.exceptions.ValidationError as err:
+    print("Invalid!")
+
+## Insufficient Input (Output: Invalid!)
+
+json_test = []
+
+try:
+    validate(instance=json_test, schema=schema)
+    print("Valid!")
+except jsonschema.exceptions.ValidationError as err:
+    print("Invalid!")
