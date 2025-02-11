baca767d8eb4250f6862fddea8fe9c305aa1170d
Ally Ryan <allyryan422@gmail.com>
2025-02-11 11:40:29 -0600

    student form creation

---
+++ react/src/pages/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Header.css'; // Import CSS for the header if needed
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1>Student Data Form</h1>
+      <p>Please fill in the information below to submit student data.</p>
+    </header>
+  );
+};
+
+export default Header;
