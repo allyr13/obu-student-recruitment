eb35a613a4f7b952997ac7034cf7220d4c47b785
simon dunk <114931161+simon-dunk@users.noreply.github.com>
2025-02-17 15:32:13 -0600

    Passing string csv data to flask. Parsing csv string to df. Includes tests (#35)
    * Passing string csv data to flask. Parsing csv string to df. Includes tests
    
    * added leading '/api' to api routes and testing script for ease
    
    * Adjusted testing
    
    * Ticket 14 15 merge resolve (#36)
    
    * Adjusted testing
    
    * merge resolve
    
    * Create conda troubleshooting documentation (#37)
    
    * Added documentation for our conda env
    
    * README update
    
    * Documentation and script adjustment

--- api/flask_server.py
+++ api/flask_server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from json_loader import get_config, edit_json_data
 import json
+from process_csv import convertCSVToDataFrame
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -25,6 +26,21 @@ def batch_job_api():
     print(request)
     response = jsonify({"message": "File successfully uploaded", "status": 200})
     return response
+
+
+@app.route('/api/upload_csv', methods=['POST'])
+def upload_csv():
+    try:
+        csv_data = request.data.decode('utf-8')
+        if not csv_data or csv_data == None:
+            return jsonify({"error": "No CSV data received", "status": 400})
+        
+        df = convertCSVToDataFrame(csv_data)
+        print(df)
+
+        return jsonify({"message": "CSV file received and saved successfully", "status": 200})
+    except Exception as e:
+        return jsonify({"error": str(e), "status": 500})
 
 
 if __name__ == '__main__':
--- api/tests/test_flask_server.py
+++ api/tests/test_flask_server.py
@@ -2,31 +2,54 @@ import unittest
 import requests
 
 class TestDemo(unittest.TestCase):
+    global HOST
+    global PORT
+    HOST = "127.0.0.1"
+    PORT = "5555"
     
     @classmethod
     def setUpClass(cls):
         """Test if we can establish a connection to the server."""
         try:
-            r = requests.get("http://0.0.0.0:6000/")
-            r.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+            r = requests.get(f"http://{HOST}:{PORT}/")
+            r.raise_for_status()
             print("Connection to the server established successfully.")
         except requests.exceptions.RequestException as e:
             print("Failed to connect to the server:", e)
             cls.skip_tests = True  # Set a flag to skip tests
-            print("Skipping tests due to failed connection.")  # Print a notification
+            print("Skipping tests due to failed connection.")
             raise unittest.SkipTest("Skipping tests due to failed connection.")
     
     def test_home(self):
-        self.assertFalse(hasattr(self, 'skip_tests'))  # Ensure skip_tests flag is not set
-        r = requests.get("http://0.0.0.0:6000/")
+        self.assertFalse(hasattr(self, 'skip_tests'))
+        r = requests.get(f"http://{HOST}:{PORT}/")
         self.assertEqual(r.text, "Hello, Flask!")
 
     def test_successful_get(self):
-        self.assertFalse(hasattr(self, 'skip_tests'))  # Ensure skip_tests flag is not set
+        self.assertFalse(hasattr(self, 'skip_tests'))
         header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.get("http://0.0.0.0:6000/get_example", headers=header)
+        r = requests.get(f"http://{HOST}:{PORT}/get_example", headers=header)
         data = r.json() 
         self.assertTrue(data["status"] == 200)
+
+    def test_csv_upload_success(self):
+        self.assertFalse(hasattr(self, 'skip_tests'))
+
+        # Hard code csv data to avoid file path errors
+        csv_data = "first,last,sex,age\njohn,doe,male,36\njane,smith,female,32\nmark,jackson,male,20\nclark,kent,male,50"
+
+        header = {'Content-type': 'text/csv', 'Accept': 'text/plain'}
+        r = requests.post(f"http://{HOST}:{PORT}/api/upload_csv", headers=header, data=csv_data)
+        data = r.json() 
+        self.assertTrue(data["status"] == 200)
+
+    def test_csv_upload_no_payload(self):
+        self.assertFalse(hasattr(self, 'skip_tests'))
+
+        header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(f"http://{HOST}:{PORT}/api/upload_csv", headers=header, data=None)
+        data = r.json() 
+        self.assertTrue(data["status"] == 400)
 
 if __name__ == '__main__':
     unittest.main()
